868dd02948e3f0f0444418fe40e9a30af73eea8f
Refnaldy <irvanrefnaldy98@gmail.com>
2018-11-07 10:32:24 +0700

    initial commit

---
+++ src/components/Sidebar.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import {} from "react-onsenui";
+
+class Sidebar extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default Sidebar;
